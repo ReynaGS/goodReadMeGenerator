eb11cf78984476229caf24322716cc5f8012e6ab
reyna garcia <garcias.reyna@gmail.com>
2020-10-21 21:42:36 -0400

    added inquirer and questions[]

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+// dependencies
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
-
+  'What is your project title?',
+  'What is your project about?',
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-
+  for (var i = 0; i < questions.length; i++) {
+    inquirer.prompt({
+      message: questions[i],
+      name: 'answer',
+    });
+  }
 }
 
 // function call to initialize program
