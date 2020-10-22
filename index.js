2b529e8a297fe89a88098e414b94b69e04020625
reyna garcia <garcias.reyna@gmail.com>
2020-10-21 22:31:23 -0400

    made an object out question variable and then passed it to the inquirer

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-  'What is your project title?',
-  'What is your project about?',
+  {
+    name: 'title',
+    type: 'input',
+    message: 'What is your project title? \n',
+  },
+
+  {
+    name: 'description',
+    type: 'input',
+    message: 'What is your project about? \n',
+  },
 ];
 
 // function to write README file
@@ -12,12 +21,13 @@ function writeToFile(fileName, data) {}
 
 // function to initialize program
 function init() {
-  for (var i = 0; i < questions.length; i++) {
-    inquirer.prompt({
-      message: questions[i],
-      name: 'answer',
-    });
-  }
+  // for loop to iterate through var question.
+  //   for (var i = 0; i < questions.length; i++) {
+  //     //inquirer to prompt user for answers.This is a promise function,
+  //     //waiting for an action to be done by the user in a given time in the future.
+  inquirer.prompt(questions).then(function (answer) {
+    console.log(answer);
+  });
 }
 
 // function call to initialize program
